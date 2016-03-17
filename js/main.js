0bbc32048f4cd4ed0833d68d7660305c1ddec130
samzah <szahir@hotmail.com>
2016-03-17 16:30:50 -0400

    done-zo

--- js/main.js
+++ js/main.js
@@ -1,18 +1,16 @@
 var $ul = $('ul');
 
-
 grains.forEach(function (grain) {
-var $li = $('<li>');
-var $name = $('<h2>');
-var $img = $('<img>');
-var $p = $('<p>');
+  var $li = $('<li>');
+  var $name = $('<h2>');
+  var $img = $('<img>');
+  var $p = $('<p>');
 
-$name.html(grain.name);
-$img.attr('src', 'images/' + grain.img);
-$p.html(grain.desc);
+  $name.html(grain.name);
+  $img.attr('src', 'images/' + grain.img);
+  $p.html(grain.desc);
 
-$li.append($img, $name, $p);
-$ul.append($li);
+  $li.append($img, $name, $p);
+  $ul.append($li);
 
 });
-
